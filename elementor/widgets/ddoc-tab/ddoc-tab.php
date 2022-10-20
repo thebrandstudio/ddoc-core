e1a5b5588949d16e028b04cc87bc93de1f7b64db
thebrandstudio <allan@bstt.mx>
2022-10-20 13:21:34 -0500

    Update ddoc-tab.php

--- elementor/widgets/ddoc-tab/ddoc-tab.php
+++ elementor/widgets/ddoc-tab/ddoc-tab.php
@@ -187,7 +187,7 @@ class DRTH_ESS_Ddoc_Tab extends Widget_Base {
                                             <?php if ( !empty($main_doc['doc']->post_title) ) : ?>
                                                 <h3> <?php echo wp_kses_post($main_doc['doc']->post_title); ?> </h3>
                                             <?php endif; ?>
-																						<p style="font-size: 24px; font-weight: 300;">
+																						<p id="descripcion" style="font-size: 24px;">
 																							<?php $shortcode = get_post_meta($main_doc['doc']->ID,'descripcion',true);
 																								echo do_shortcode($shortcode);?>
 																						</p>
