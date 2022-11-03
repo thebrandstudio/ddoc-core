7bf510c2266080217f2a73024d83a7f7d007e7fb
thebrandstudio <allan@bstt.mx>
2022-11-03 16:15:45 -0600

    Update ddoc-tab.php

--- elementor/widgets/ddoc-tab/ddoc-tab.php
+++ elementor/widgets/ddoc-tab/ddoc-tab.php
@@ -129,7 +129,7 @@ class DRTH_ESS_Ddoc_Tab extends Widget_Base {
                                     $active = ($i == 0) ? 'active' : '';
                                     $doc_name = explode( ' ', $doc->post_title );
                                     ?>
-                                    <li id="icono" class="nav-item" data-id="#doc-<?php echo esc_attr($doc->ID); ?>">
+                                    <li id="icono-<?php echo esc_attr($doc->ID); ?>" class="nav-item" data-id="#doc-<?php echo esc_attr($doc->ID); ?>">
 																			<a class="nav-link <?php echo 'doc-'.esc_attr($doc->ID).' '; echo esc_attr($active) ?>" data-toggle="tab" href="#doc-<?php echo esc_attr($doc->ID) ?>">
 																				<div class="img post-icon-<?php echo esc_attr($doc->ID); ?>" style="height: 56px; width: 56px; margin-left: auto; margin-right: auto;">
 																						<?php echo $this->get_ddoc_icon($doc->ID);  ?>
